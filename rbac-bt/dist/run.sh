19d86843eb794560ff4a5e4bf49d8ba61640f3d8
damascenodiego <carlos.damasceno08@gmail.com>
2015-06-24 19:04:57 -0300

    run.sh updated
    Former-commit-id: 8d905d5285d16b2413689f7e89ea980248e92d17

--- rbac-bt/dist/run.sh
+++ rbac-bt/dist/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-java -jar -Xmx2048m rbacBt.jar -r2f  ./$1.rbac -o ./$1
-java -jar -Xmx2048m rbacBt.jar -rmut ./$1.rbac -o ./$1
-#java -jar -Xmx2048m rbacBt.jar -qset ./$1/$1.fsm  -o ./$1
-#java -jar -Xmx2048m rbacBt.jar -pset ./$1/$1.fsm  -o ./$1
-#java -jar -Xmx2048m rbacBt.jar -tt   ./$1/$1.fsm  -o ./$1
+FNAME=`echo $1 | cut -d'.' -f1`
+java -jar -Xmx2048m rbacBt.jar -r2f  ./$FNAME.rbac -o ./$FNAME
+java -jar -Xmx2048m rbacBt.jar -rmut ./$FNAME.rbac -o ./$FNAME
+#java -jar -Xmx2048m rbacBt.jar -qset ./$FNAME/$FNAME.fsm  -o ./$FNAME
+#java -jar -Xmx2048m rbacBt.jar -pset ./$FNAME/$FNAME.fsm  -o ./$FNAME
+#java -jar -Xmx2048m rbacBt.jar -tt   ./$FNAME/$FNAME.fsm  -o ./$FNAME
