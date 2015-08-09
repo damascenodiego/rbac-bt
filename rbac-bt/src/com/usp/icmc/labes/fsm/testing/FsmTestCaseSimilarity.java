2a3c091459b589f90248abf392fd89c22259c203
damascenodiego <carlos.damasceno08@gmail.com>
2015-08-09 17:09:49 -0300

    trying to optimize cartaxo...
    Former-commit-id: c283c6e52a74a2ce623b0c3cb2f8f9c03746776f

--- rbac-bt/src/com/usp/icmc/labes/fsm/testing/FsmTestCaseSimilarity.java
+++ rbac-bt/src/com/usp/icmc/labes/fsm/testing/FsmTestCaseSimilarity.java
@@ -3,6 +3,7 @@ package com.usp.icmc.labes.fsm.testing;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -30,6 +31,7 @@ public class FsmTestCaseSimilarity {
 		return instance;
 	}
 
+
 	public static void sortSimilarityCartaxo(FsmModel spec, FsmTestSuite testSuite) {
 		FsmSUT sut = new FsmSUT(spec);
 		FsmState initState = sut.getCurrentState();
@@ -46,85 +48,75 @@ public class FsmTestCaseSimilarity {
 
 		}
 
-		double [][] distMatrix = generateDistMatrix(testList);
+		//		List<PairTestSimilarity> distMatrix = generateDistMatrix(testList);
+		List<PairTestSimilarity> distMatrix = generateDistMatrix(testList);
 
 		double dist_ij = 0; 
 
-		List<Integer> s = new ArrayList<Integer>();
-		for (int k = 0; k < testList.size(); k++)  s.add(k);
+		List<FsmTestCase> s = new ArrayList<FsmTestCase>();
+		for (int k = 0; k < testList.size(); k++)  s.add(testList.get(k));
+		Collections.sort(distMatrix);
+		List<FsmTestCase> l = new ArrayList<FsmTestCase>();
+//		for (PairTestSimilarity gtSim : distMatrix) {
 
-		List<Integer> l = new ArrayList<Integer>();
-		while (true) {
-			if(s.isEmpty() || s.size() == 1){
-				l.add(s.get(0));
-				s.remove(0);
-				break;
-			}
-			double greaterDist = -1;
-			int greater_i = -1;
-			int greater_j = -1;
-			for (int k = 0; k < s.size()-1; k++) {
-				for (int k2 = k+1; k2 < s.size(); k2++) {
-					dist_ij = getDist(s.get(k),s.get(k2),distMatrix);
-					if(dist_ij>=greaterDist){
-						greaterDist = dist_ij;
-						greater_i = (k);
-						greater_j = (k2);
-					}
-				}
-			}
-			FsmTestCase tc_k = testList.get(s.get(greater_i));
-			FsmTestCase tc_k2 = testList.get(s.get(greater_j));
-			
-			
-			if(tc_k.getPath().size()>tc_k2.getPath().size()){
-				l.add(s.get(greater_j));
-				s.remove(greater_j);
-			}else if(tc_k.getPath().size()<tc_k2.getPath().size()){
-				l.add(s.get(greater_i));
-				s.remove(greater_i);
-			}else if(RandomGenerator.getInstance().getRnd().nextBoolean()){
-				l.add(s.get(greater_i));
-				s.remove(greater_i);
-			}else{
-				l.add(s.get(greater_j));
-				s.remove(greater_j); 
-			}
-//			System.out.println(greaterDist);
+//		}
+//
+		PairTestSimilarity gtSim = null;
+		while (!distMatrix.isEmpty()) {
+			gtSim = distMatrix.get(0);
+
+			FsmTestCase selTest = null;
+			if(gtSim.hasEqualSize()){
+				selTest = gtSim.getTestRandom();
+			}else selTest = gtSim.getTestLonger();
+			removePairsWithTestCase(distMatrix,selTest);
+			l.add(selTest);
+			distMatrix.remove(gtSim);
+			System.out.println(gtSim.getSimilarity());
 		}
+		if(!l.contains(gtSim.getTestLonger())) l.add(gtSim.getTestLonger());
+		if(!l.contains(gtSim.getTestShorter())) l.add(gtSim.getTestShorter());
+		
 		testSuite.getTestCases().clear();
-		for (int i = l.size()-1; i >= 0; i--) {
-			int index = l.get(i);
-			testSuite.getTestCases().add(testList.get(index));
-		}
-//		System.out.println(calcNit(testSuite.getTestCases().get(0), testSuite.getTestCases().get(1)));
+		testSuite.getTestCases().addAll(l);
 	}
 
-	private static double getDist(int i, int j, double[][] distMatrix) {
+	private static void removePairsWithTestCase(List<PairTestSimilarity> distMatrix, FsmTestCase selTest) {
+		Set<PairTestSimilarity> toRemove = new HashSet<PairTestSimilarity>();
+		for (PairTestSimilarity pairTestSimilarity : distMatrix) {
+			if(pairTestSimilarity.getTest01().equals(selTest) || pairTestSimilarity.getTest00().equals(selTest)) toRemove.add(pairTestSimilarity);
+		}
+		distMatrix.removeAll(toRemove);
+	}
+
+	public static double getDist(int i, int j, double[][] distMatrix) {
 		int real_i = Math.min(i, j); 
 		int max = Math.max(i, j);
 		int real_j = max - real_i -1;
 		return distMatrix[real_i][real_j];
 	}
 
-	private static double[][] generateDistMatrix(List<FsmTestCase> testList) {
-		double [][] distMatrix = new double[testList.size()-1][];
+	//	private static double[][] generateDistMatrix(List<FsmTestCase> testList) {
+	private static List<PairTestSimilarity> generateDistMatrix(List<FsmTestCase> testList) {
+		//		double [][] distMatrix = new double[testList.size()-1][];
+		List<PairTestSimilarity> pairsTest = new ArrayList<PairTestSimilarity>();
 
 		for (int i = 0; i < testList.size()-1; i++) {
 			FsmTestCase tci = testList.get(i);
-			distMatrix[i] = new double[testList.size()-i-1];
+			//			distMatrix[i] = new double[testList.size()-i-1];
 			for (int j = i+1; j < testList.size(); j++) {
 				FsmTestCase tcj = testList.get(j);
-				int nit = calcNit(tci,tcj);
-				double avgij = (tci.getPath().size()+tcj.getPath().size())/2.0;
-				double similarityij = nit/avgij;
-				distMatrix[i][j-i-1] = similarityij;
+				//				int nit = calcNit(tci,tcj);
+				//				double avgij = (tci.getPath().size()+tcj.getPath().size())/2.0;
+				//				double similarityij = nit/avgij;
+				//				distMatrix[i][j-i-1] = similarityij;
+				pairsTest.add(new PairTestSimilarity(tci, tcj));
 			}
 		}
-		return distMatrix;
+		return pairsTest;
 	}
 
-	private static int calcNit(FsmTestCase tci, FsmTestCase tcj) {
+	public int calcNit(FsmTestCase tci, FsmTestCase tcj) {
 		Set<FsmTransition> tcNit = new HashSet<FsmTransition>();
 		tcNit.addAll(tci.getPath());
 		tcNit.retainAll(tcj.getPath());
@@ -148,5 +140,68 @@ public class FsmTestCaseSimilarity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+}
+
+class PairTestSimilarity implements Comparable<PairTestSimilarity>{
+
+	FsmTestCase test00;
+	FsmTestCase test01;
+	double similarity;
+
+	public PairTestSimilarity(FsmTestCase tci, FsmTestCase tcj) {
+		int nit = FsmTestCaseSimilarity.getInstance().calcNit(tci,tcj);
+		double avgij = (tci.getPath().size()+tcj.getPath().size())/2.0;
+		similarity = nit/avgij;
+		if(tci.getPath().size()<tcj.getPath().size()){
+			this.test00 = tci;
+			this.test01 = tcj;
+		}else if(tci.getPath().size()>tcj.getPath().size()){
+			this.test00 = tcj;
+			this.test01 = tci;
+		}else if(RandomGenerator.getInstance().getRnd().nextBoolean()){
+			this.test00 = tci;
+			this.test01 = tcj;
+
+		}else{
+			this.test00 = tcj;
+			this.test01 = tci;
+
+		}
+	}
+	public FsmTestCase getTestShorter() {
+		return test00;
+	}
+
+	public FsmTestCase getTestLonger() {
+		return test01;
+	}
+
+	public boolean hasEqualSize() {
+		return test00.getPath().size()==test01.getPath().size();
+	}
+
+	public FsmTestCase getTestRandom() {
+		if(RandomGenerator.getInstance().getRnd().nextBoolean()){
+			return test00;
+		}
+		return test01;
+	}
+
+	public FsmTestCase getTest00() {
+		return test00;
+	}
+
+	public FsmTestCase getTest01() {
+		return test01;
+	}
+
+	public double getSimilarity() {
+		return similarity;
+	}
+	@Override
+	public int compareTo(PairTestSimilarity o) {
+		return Double.compare(o.similarity, this.similarity);
 	}
 }
